85baf888541d9a538e205055ed2bc63d309686c9
Olivia Gautheron <oxtramag@hotmail.fr>
2022-10-23 21:28:00 +0200

    add:React-table

--- src/pages/arrayEmployee/ArrayEmployee.jsx
+++ src/pages/arrayEmployee/ArrayEmployee.jsx
@@ -1,176 +1,154 @@
 import { useSelector } from 'react-redux';
-import { useState } from 'react'
+import React,{ useMemo } from 'react'
 import './ArrayEmployee.css'
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass, faBars, faArrowUp, faArrowDown, faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
-//import SearchBar from '../../components/listOfEmployee/searchBar/searchBar';
+import { faMagnifyingGlass, faArrowUp, faArrowDown, faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import  COLUMNS  from './columns';
+import { useTable, useSortBy, usePagination, useFilters, useGlobalFilter, useAsyncDebounce } from 'react-table';
+
+function GlobalFilter({globalFilter, setGlobalFilter}) {
+    const [value, setValue] = React.useState(globalFilter)
+    const onChange = useAsyncDebounce(value => {
+            setGlobalFilter(value || undefined)
+            }, 200)
+    
+    return (
+        <span>Search:{''}
+                    <input
+                        value={value || ""}
+                        onChange={(e) => {
+                            setValue(e.target.value);
+                            onChange(e.target.value);
+                        }}
+                        placeholder= 'Search...'
+                        className='header__search__input'
+                    />
+                    <FontAwesomeIcon icon={faMagnifyingGlass} className='header__search__icone' />
+        </span>
+    )
+}
 
 const ArrayEmployee = () => {
+    const employee = useSelector((state)=>state.employee)
+
+    const columns = useMemo(() => COLUMNS, [])
+    const data = employee
     
-    const data = useSelector((state)=>state.employee)
+    const tableInstance = useTable({
+        columns: columns,
+        data: data,
+    },
+    useGlobalFilter,
+    useFilters,
+    useSortBy,
+    usePagination
+    )
 
-    console.log('data', data);
-    
-    const [employeesPerTable, setEmployeesPerTable] = useState(10);
-    const [indexStart, setIndexStart] = useState(0);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [searchData, setSearchData] = useState('');
-    const [filteredData, setFilteredData] = useState('firstName');
-    const [columns, setColumns] = useState([
-		{ title: 'First Name', data: 'firstName', state: 'asc' },
-		{ title: 'Last Name', data: 'lastName', state: '' },
-		{ title: 'Start Date', data: 'startDate', state: '' },
-		{ title: 'Department', data: 'department', state: '' },
-		{ title: 'Date of Birth', data: 'dateOfBirth', state: '' },
-		{ title: 'Street', data: 'street', state: '' },
-		{ title: 'City', data: 'city', state: '' },
-		{ title: 'State', data: 'state', state: '' },
-		{ title: 'Zip Code', data: 'zipCode', state: '' }
-	]);
+    const { 
+        getTableProps, 
+        getTableBodyProps, 
+        headerGroups, 
+        rows, 
+        prepareRow, 
+        setPageSize, 
+        canPreviousPage, 
+        state, 
+        canNextPage, 
+        nextPage, 
+        previousPage, 
+        setGlobalFilter,
+        state: { pageIndex,  pageSize}, } = tableInstance
 
-    function handleChangeEmployeesPerTable (e) {
-        setEmployeesPerTable(e.target.value);
-        setCurrentPage(1)
-        setIndexStart(0)
-    }
 
-    function nextPage() {
-        if (currentPage < Math.round(data.length/employeesPerTable)) {
-			setCurrentPage(currentPage + 1);
-			setIndexStart(indexStart + employeesPerTable);
-        }
-    }
-
-    function prevPage() {
-        if (currentPage > 1) {
-			setCurrentPage(currentPage - 1);
-			setIndexStart(indexStart - employeesPerTable);
-		}
-    }
-
-    function changeStateColumn(changeColumn) {
-        const columnsTemplate = [...columns]
-        columnsTemplate.forEach((column) => {
-            if (column.data === changeColumn) {
-				if (column.state === '') {
-					column.state = 'asc';
-				} else if (column.state === 'asc') {
-					column.state = 'desc';
-				} else if (column.state === 'desc') {
-					column.state = 'asc';
-				}
-			} else {
-				column.state = '';
-			}
-		});
-        setColumns(columnsTemplate);
-		setFilteredData(changeColumn);
-    }
-
-    function displayData() {
-        return data
-                .filter((item) => {
-                    return Object.keys(item).some((key) => {
-                        return item[key].toString().toLowerCase().includes(searchData);
-                    });
-                })
-                .sort((a,b) => {
-                    if(filteredData === 'zipCode') {
-                        return columns.find((column) => column.data === filteredData).state === 'asc' ? a[filteredData] - b[filteredData] : b[filteredData] - a[filteredData];
-                    } else {
-                        return columns.find((column) => column.data === filteredData).state === 'asc' ? a[filteredData].localeCompare(b[filteredData]) : b[filteredData].localeCompare(a[filteredData]);
-                    }
-                });
-    }
-
-    return (
-
-    <div className="table">
+    return(
+        <div className="table">
         <h1 className='table__title'>Current Employees</h1>
         <NavLink to='/' className="table__link">Return Home</NavLink>
         <div className='table__header'>
-
-            {/* select number entries */}
             <div className='table__header__entries'>
                 <span>Show</span>
-                <select className='table__header__select' onChange={handleChangeEmployeesPerTable}>
-                    <option value='10'>10</option>
-                    <option value='20'>20</option>
-                    <option value='50'>50</option>
-                    <option value='100'>100</option>
+                <select value={pageSize}
+                        className='table__header__select'
+                        onChange={e => {
+                            setPageSize(Number(e.target.value))
+                        }}
+                        >
+                        {[10, 20, 25, 50, 100].map(pageSize => (
+                <option key={pageSize} value={pageSize}>{pageSize}</option>
+                ))}
                 </select>
                 <span>entries</span>
             </div>
-            
-            {/* searchbar */}
             <div className='table__header__search'>
-                <label htmlFor='table-search' >Search</label>
-                <input type='text' id='table-search' className='header__search__input' placeholder='Search...' onChange={(e) => setSearchData(e.target.value)} />
-                <FontAwesomeIcon icon={faMagnifyingGlass} className='header__search__icone' />
-
+                <GlobalFilter
+                    globalFilter={state.globalFilter}
+                    setGlobalFilter={setGlobalFilter}
+                />
             </div>
         </div>
-
-        {/*table */}
-        <table className='table__content'>
-        
-            <thead className='table__content__container'>
-                    <tr className='table__content__tr'>
-                        {columns.map((column, idx) => (
-                            <th key={'colum-'+idx} className='table__content__label'>
-                                <button className='table__content__button'
-                                        onClick={() => changeStateColumn(column.data)}
-                                >
-                                    {column.title}
-                                    <div>
-                                        {column.state === '' && <FontAwesomeIcon icon={faBars} className='table__content__icon' /> }
-                                        {column.state === 'asc' && <FontAwesomeIcon icon={faArrowUp} className='table__content__icon' />}
-                                        {column.state === 'desc' && <FontAwesomeIcon icon={faArrowDown} className='table__content__icon' />}
-                                    </div>
-                                </button>
+            
+        <table {...getTableProps()}>
+            <thead>
+                {
+                    headerGroups.map(headerGroup => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                    {
+                        headerGroup.headers.map((column) => (
+                            <th {...column.getHeaderProps(column.getSortByToggleProps())}>{column.render('Header')}
+                                <span>
+                                    {column.isSorted
+                                        ? column.isSortedDesc
+                                        ? <FontAwesomeIcon icon={faArrowDown} className='table__content__icon' />
+                                        : <FontAwesomeIcon icon={faArrowUp} className='table__content__icon' />
+                                    : ''}
+                                </span>
                             </th>
-                    ))}
-                    </tr>
+                        ))
+                    }
+                </tr>
+
+                    ))
+                }
             </thead>
-            <tbody className='table__content__body'>
-                {displayData().slice(indexStart, indexStart + employeesPerTable).map((row, idx) => (
-                    <tr key={'user-'+idx} className='table__content__tr'>
-                        <td className='table__content__td'>{row.firstName}</td>
-                        <td className='table__content__td'>{row.lastName}</td>
-                        <td className='table__content__td'>{row.startDate}</td>
-                        <td className='table__content__td'>{row.department}</td>
-                        <td className='table__content__td'>{row.dateOfBirth}</td>
-                        <td className='table__content__td'>{row.street}</td>
-                        <td className='table__content__td'>{row.city}</td>
-                        <td className='table__content__td'>{row.state}</td>
-                        <td className='table__content__td'>{row.zipCode}</td>
-                    </tr>
-                ))}
+            <tbody {...getTableBodyProps()}>
+                {
+                    rows.map(row => {
+                        prepareRow(row)
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {
+                                    row.cells.map((cell) => {
+                                        return <td {...cell.getCellProps()}>
+                                            {cell.render('Cell')}
+                                        </td>
+                                    })
+                                }
+                            </tr>
+                        )
+                    })
+                }
             </tbody>
         </table>
-
         <nav className='table__nav' aria-label='table navigation'>
-            {/*Show number employees*/}
-            <span className='table__nav__text'>Showing
-            <span >{Number(indexStart+1)} - {Number(indexStart + employeesPerTable) < displayData().length ? Number(indexStart + employeesPerTable) : displayData().length}</span>
-            of
-            <span >{displayData().length}</span>
-            </span>
+        
+        <span className='table__nav__text'>Showing<span >{Number(pageIndex + 1)}</span>to<span>{rows.length}</span>
+            of 
+            <span >{rows.length}</span>
+        </span>
 
-            {/*Show number page*/}
-            <ul className='table__nav__list'>
+        <ul className='table__nav__list'>
                 <li>
-                    <button className='table__nav__list__button' onClick={prevPage} >  
-                        <FontAwesomeIcon icon={faArrowLeft} className='table__nav__list__icon--left' />                      
+                    <button className='table__nav__list__button' onClick={() => previousPage()} disabled={!canPreviousPage}>  
+                        {<FontAwesomeIcon icon={faArrowLeft} className='table__nav__list__icon--left' />  }                    
                         <span>Previous</span> 
                     </button>
                 </li>
                 <li>
-                    <p className='table__nav__list__text'>{currentPage}</p>
+                    <p className='table__nav__list__text'>{pageIndex + 1}</p>
                 </li>
                 <li>
-                    <button className='table__nav__list__button' onClick={nextPage}>
+                    <button className='table__nav__list__button' onClick={() => nextPage()} disabled={!canNextPage}>
                         <span>Next</span>
                         <FontAwesomeIcon icon={faArrowRight} className='table__nav__list__icon--right' />
                     </button>
@@ -178,8 +156,6 @@ const ArrayEmployee = () => {
             </ul>
         </nav>
     </div>
-    )};
-
-
+)}
 
 export default ArrayEmployee
